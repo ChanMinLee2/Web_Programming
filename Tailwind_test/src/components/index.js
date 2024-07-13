e5ca37fe0c8722ff226ca3633bf373b93df805c7
chanmin Lee <dlcksals87@gmail.com>
2024-07-13 23:03:13 +0900

    🎨 FEAT : tailwind test - index.js 로 component 동시 export

---
+++ Tailwind_test/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Header } from "./Header";
+export { default as Hero } from "./Hero";
+export { default as TopDestination } from "./TopDestination";
+export { default as Features } from "./Features";
+export { default as AppPromo } from "./AppPromo";
+export { default as Footer } from "./Footer";
