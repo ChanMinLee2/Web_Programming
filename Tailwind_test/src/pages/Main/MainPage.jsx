29a948fc34c7c7c45799e7711d6c3624ef2ef07e
chanmin Lee <dlcksals87@gmail.com>
2024-07-14 17:32:45 +0900

    🎨 FEAT : tailwind test - MainPage skeleton

--- Tailwind_test/src/pages/Main/MainPage.jsx
+++ Tailwind_test/src/pages/Main/MainPage.jsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import {
+  Header,
+  Hero,
+  TopDestination,
+  Features,
+  AppPromo,
+  Footer,
+} from "../../components";
 function MainPage() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <TopDestination />
+      <Features />
+      <AppPromo />
+      <Footer />
+    </div>
+  );
 }
 
 export default MainPage;
