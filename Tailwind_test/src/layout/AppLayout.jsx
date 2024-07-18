8a64843d85fc04883cfac5393bf4220c7122aeec
chanmin Lee <dlcksals87@gmail.com>
2024-07-18 23:53:11 +0900

    🎨 FEAT : tailwind test - AppLayout.jsx upload

--- Tailwind_test/src/layout/AppLayout.jsx
+++ Tailwind_test/src/layout/AppLayout.jsx
@@ -6,7 +6,7 @@ function AppLayout() {
   // Outlet 위치에 내가 지정한 Router가 작동한다.
   return (
     <S.AppContainer>
-      <Navbar />
+      {/* <Navbar />s */}
       <S.OutletWrapper>
         <Outlet />
       </S.OutletWrapper>
